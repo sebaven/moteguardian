158ff55e5723a6320d785d026d74118e17702e70
carogalli <carogalli@7c7263cb-3a55-0410-952c-2bdb4ee20446>
2008-08-28 00:57:10 +0000

    limpiando la aplicación

--- trunk/acciones/planificacion/accion.SalidaPopupPlanificacion.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class SalidaPopupPlanificacion extends Action {
-	var $tpl = "tpl/planificacion/tpl.SalidaPopupPlanificacion.php";
-}
-?>
--- trunk/batch/srdf_stop.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-
-/usr/bin/php5 ./srdf_control.php detener  > ./log/srdf_stop.out
-
