8bacd16d193336ff8526703ac2fb680bc3e37f8e
yoelseba <yoelseba@7c7263cb-3a55-0410-952c-2bdb4ee20446>
2008-08-30 22:07:14 +0000

    Cambios iniciales... TODO: CAMBIAR VERISÓN DEL FRAMEWORK!!!

--- trunk/comun/inc.menu.php
+++ trunk/comun/inc.menu.php
@@ -5,17 +5,23 @@ if( RegistryHelper::isUserLogged() ){
 <ul>
 	<li><h2>Usuario</h2>
 		<ul>
-<? 
-	if( RegistryHelper::getRolUsuario()->id==ROL_ID_ADMIN ){
-?>
-			<li><a href="index.php?accion=usuario_adm">Administraci&oacute;n</a></li>
-			<li><a href="index.php?accion=usuario_new">Alta</a></li>
-<? 
-	} 
-?>
+			<? if( RegistryHelper::getRolUsuario()->id==ROL_ID_ADMIN ){	?>
+				<li><a href="index.php?accion=usuario_adm">Administraci&oacute;n</a></li>
+				<li><a href="index.php?accion=usuario_new">Alta</a></li>
+			<? } ?>
       		<li><a href="index.php?accion=cambiar_pass">Cambiar Contrase&ntilde;a</a></li>
     	</ul>
   	</li>
+</ul>
+<ul>
+	<li><h2>Salas</h2>		
+    	<ul>
+			<? if( RegistryHelper::getRolUsuario()->id==ROL_ID_ADMIN ){	?>
+				<li><a href="index.php?accion=sala_select&redir=usuario_new&prefijoTitle=Configurar%20">Configuraci&oacute;n</a></li>			
+			<? } ?>
+      		<li><a href="index.php?accion=sala_select&redir=usuario_adm&prefijoTitle=Supervisar%20">Consulta</a></li>
+		</ul>
+	</li>
 </ul>
 <ul>
 	<li><h2>Sistema</h2>
