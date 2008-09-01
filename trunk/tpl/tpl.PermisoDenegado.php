a69575b601f579b140f996937c8b52943a72ad46
carogalli <carogalli@7c7263cb-3a55-0410-952c-2bdb4ee20446>
2008-09-01 02:17:25 +0000

    Borre la carpeta de tpls

--- trunk/tpl/tpl.PermisoDenegado.php
+++ trunk/tpl/tpl.PermisoDenegado.php
@@ -1,20 +1,23 @@
 <table border="0" bgcolor="#FFFFFF" width="100%" height="50%">
-	<tr>
-		<td bgcolor="#FFFFFF" align="center" valign="middle">
-  			<br/><br/><br/>		
-	  		<h1 align="center">
-	  			<font color="red" face="Trebuchet MS">Permiso Denegado</font>
-	  		</h1>
-	  		<p>
-		  		<?=$errores ?>
-		  		<?=$mensaje ?>
-		  		<br/><br/><br/>
-			</p>
-		</td>
-	</tr>
-	<tr>
-		<td align="center" valign="middle">
-			<a href="javascript:history.back()">Volver</a>
-		</td>
-	</tr>	    
+<tr>
+	<td bgcolor="#FFFFFF" align="center" valign="middle">
+  		<br/>
+  		<br/>
+  		<br/>		
+  		<br/>		
+  		<h2 align="center" style="vertical-align: middle;">
+  			<font color="red" face="Trebuchet MS"> <img src="imagenes/warning.gif" width="28px" style="vertical-align: middle;"/> Permiso Denegado</font>
+  		</h2>
+  		<?=$errores ?>
+  		<?=$mensaje ?>
+  		<br/>
+  		<br/>
+  		<br/>
+	</td>
+</tr>
+<tr>
+	<td align="center" valign="middle">
+		<a href="javascript:history.back()">Volver</a>
+	</td>
+</tr>	    
 </table>
