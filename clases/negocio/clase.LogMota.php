27e9b237cf581ca01bf6d9c633703919f0cbba06
carogalli <carogalli@7c7263cb-3a55-0410-952c-2bdb4ee20446>
2008-09-02 01:53:51 +0000

    Clase log mota

---
+++ clases/negocio/clase.LogMota.php
@@ -0,0 +1,26 @@
+<?
+class LogMota extends AbstractEntity
+{
+    /**
+     * Nombre de la tabla sobre a la cual accede la clase
+     * @access protected
+     * @var string
+     */
+    var $_tablename = 'log_mota';
+
+    /**
+     * Nombre de los campos, menos el campo id
+     * @access protected
+     * @var array
+     */
+    var $_fields = array
+    (				
+		'id_mota'	=> 'int',
+		'timestamp'	=> 'varchar'
+    );
+
+    var $id;
+    var $id_mota;
+    var $timestamp;    
+}
+?>
