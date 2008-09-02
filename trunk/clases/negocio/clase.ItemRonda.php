7d0aaec7c555a10a638cbb1dd8a5f431369c628b
carogalli <carogalli@7c7263cb-3a55-0410-952c-2bdb4ee20446>
2008-09-02 01:47:37 +0000

    Clase Item Ronda

---
+++ trunk/clases/negocio/clase.ItemRonda.php
@@ -0,0 +1,30 @@
+<?
+class ItemRonda extends AbstractEntity
+{
+    /**
+     * Nombre de la tabla sobre a la cual accede la clase
+     * @access protected
+     * @var string
+     */
+    var $_tablename = 'item_ronda';
+
+    /**
+     * Nombre de los campos, menos el campo id
+     * @access protected
+     * @var array
+     */
+    var $_fields = array
+    (				
+		'id_ronda' 		=> 'int',
+		'id_sala' 		=> 'int',
+		'duracion' 		=> 'int',			
+    	'orden' 		=> 'int'      
+    );
+
+    var $id;    
+    var $id_sala;
+    var $id_ronda;
+    var $orden;
+    var $duracion;
+}
+?>
