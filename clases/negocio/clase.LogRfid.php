00669cfc72ef42dbbe3f0c6e8b0f80333268f635
carogalli <carogalli@7c7263cb-3a55-0410-952c-2bdb4ee20446>
2008-09-02 02:11:01 +0000

    clases

---
+++ clases/negocio/clase.LogRfid.php
@@ -0,0 +1,28 @@
+<?
+class LogRfid extends AbstractEntity
+{
+    /**
+     * Nombre de la tabla sobre a la cual accede la clase
+     * @access protected
+     * @var string
+     */
+    var $_tablename = 'log_rfid';
+
+    /**
+     * Nombre de los campos, menos el campo id
+     * @access protected
+     * @var array
+     */
+    var $_fields = array
+    (				
+		'id_rfid'			=> 'int',
+		'timestamp_inicio'	=> 'varchar',
+		'timestamp_fin'		=> 'varchar'
+    );
+
+    var $id;
+    var $id_mota;
+    var $timestamp_inicio;    
+    var $timestamp_fin;    
+}
+?>
