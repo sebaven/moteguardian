2f3fbfab11995202dd2f344eeb1910c1614d7ae6
carogalli <carogalli@7c7263cb-3a55-0410-952c-2bdb4ee20446>
2008-09-02 01:51:35 +0000

    Clase log alarma

---
+++ trunk/clases/negocio/clase.LogAlarma.php
@@ -0,0 +1,30 @@
+<?
+class LogAlarma extends AbstractEntity
+{
+    /**
+     * Nombre de la tabla sobre a la cual accede la clase
+     * @access protected
+     * @var string
+     */
+    var $_tablename = 'log_alarma';
+
+    /**
+     * Nombre de los campos, menos el campo id
+     * @access protected
+     * @var array
+     */
+    var $_fields = array
+    (				
+		'id_dispositivo_disparador'	=> 'int',
+		'timestamp_inicio'		 	=> 'varchar',
+		'timestamp_fin'		 		=> 'varchar',			
+    	'es_falsa' 					=> 'bool'      
+    );
+
+    var $id;    
+    var $id_dispositivo_disparador;
+    var $timestamp_inicio;
+    var $timestamp_fin;
+    var $es_false;
+}
+?>
