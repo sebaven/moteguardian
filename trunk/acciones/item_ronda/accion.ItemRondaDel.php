d9a4ce5010706601effd1e810d5eba4b2c2bf2ac
carogalli <carogalli@7c7263cb-3a55-0410-952c-2bdb4ee20446>
2008-09-04 14:14:41 +0000

    ABM ronda

---
+++ trunk/acciones/item_ronda/accion.ItemRondaDel.php
@@ -0,0 +1,27 @@
+<?php
+include_once BASE_DIR . 'clases/negocio/clase.ItemRonda.php';
+include_once BASE_DIR ."clases/listados/clase.Listados.php";
+
+class ItemRondaDel extends Action {
+	
+	function procesar(&$nextAction){
+		$itemRonda = new ItemRonda($_GET['id']);
+		$itemRonda->baja_logica = TRUE_;
+		$res = $itemRonda->save();
+		
+		$parametros['id_ronda'] = $_GET['id_ronda'];
+		$accion = 'RondaAlta';
+				
+		if ($res) {
+			$nextAction->setNextAction($accion, 'eliminacion.item.ronda.ok', $parametros);
+		}
+		else {
+			$parametros['error'] = "1";
+			$nextAction->setNextAction($accion, 'eliminacion.item.ronda.error', $parametros);
+		}
+	}
+	
+}
+
+
+?>
